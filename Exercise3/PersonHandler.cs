3bad1584799d8050eb4941f91c40655232bea741
reenhilm <christian@kajal.se>
2022-05-20 13:24:52 +0200

    Uppgift 4 klar

--- Exercise3/PersonHandler.cs
+++ Exercise3/PersonHandler.cs
@@ -1,19 +1,38 @@
 ﻿public class PersonHandler
 {
-    public Person CreatePerson(int age, string fname, string lname, double height, double weight) => CreatePerson(age, fname, lname, Convert.ToInt32(height), Convert.ToInt32(weight));
+    public Person CreatePerson(int age, string fname, string lname, double height, double weight) => CreatePerson(age, fname, lname, ConvertDoubleToInt(height), ConvertDoubleToInt(weight));
 
-    private Person CreatePerson(int age, string fname, string lname, int height, int weight)
-    {
-        var person = new Person(fname, lname)
+    private Person CreatePerson(int age, string fname, string lname, int height, int weight) => new (fname, lname)
         {
             Age = age,
             Height = height,
             Weight = weight
         };
-        return person;
-    }
+        
     public void SetAge(Person pers, int age)
     {
         pers.Age = age;
+    }
+
+    public void SetFName(Person pers, string fname)
+    {
+        pers.FName = fname;
+    }
+
+    public void SetLName(Person pers, string lname)
+    {
+        pers.LName = lname;
+    }
+
+    private int ConvertDoubleToInt(double dValue) => Convert.ToInt32(dValue);
+
+    public void SetHeight(Person pers, double height)
+    {
+        pers.Height = ConvertDoubleToInt(height);
+    }
+
+    public void SetWeight(Person pers, double weight)
+    {
+        pers.Weight = ConvertDoubleToInt(weight);
     }
 }
