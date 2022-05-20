1a233b25729a4ef829b3573aae52dcf908823439
reenhilm <christian@kajal.se>
2022-05-20 12:31:13 +0200

    Uppgift 2 klar

---
+++ Exercise3/PersonHandler.cs
@@ -0,0 +1,7 @@
+﻿internal class PersonHandler
+{
+    public void SetAge(Person pers, int age)
+    {
+        pers.Age = age;
+    }
+}
--- Exercise3/Program.cs
+++ Exercise3/Program.cs
@@ -1,13 +1,18 @@
 ﻿//Ja, jag kommer åt dom publika variablerna efter jag instantierat en Person
-//1.
+
 try
-{ 
+{
+    //1.
     var person = new Person("Christian", "Rönnholm");
     person.Age = 40;
     person.LName = "Test";
     person.FName = "Test";
     person.Weight = 100;
     person.Height = 189;
+
+    //2.
+    var personHandler = new PersonHandler();
+    personHandler.SetAge(person, 41);
 }
 catch(ArgumentException ex)
 {
